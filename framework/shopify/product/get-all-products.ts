53a081b897ce74a741477aa2ecea342abcb1afd5
Jaydenasanto <83191663+Jaydenasanto@users.noreply.github.com>
2022-07-09 11:53:32 +1000

    Section 37- 59
    Destructing the data and simplifying / normalising it

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,9 +1,21 @@
 import getAllProductsQuery from "../utils/queries/get-all-products";
 import fetchApi from "../utils/queries/fetch-api";
+import { normalizeProduct } from "../utils/normalize";
+import { ProductConnection } from "../schema";
+import { Product } from "../../common/types/product";
 
-const getAllProducts = async (): Promise<any[]> => {
-  const products = await fetchApi({ query: getAllProductsQuery });
-  return products.data;
+type ReturnType = {
+  products: ProductConnection;
+};
+
+const getAllProducts = async (): Promise<Product[]> => {
+  const { data } = await fetchApi<ReturnType>({ query: getAllProductsQuery });
+
+  const products =
+    data.products.edges.map(({ node: product }) => normalizeProduct(product)) ??
+    [];
+
+  return products;
 };
 
 export default getAllProducts;
