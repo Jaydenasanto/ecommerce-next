1c77a3e8134d3b8e4a8830b1fe43e721f2964c23
Jaydenasanto <83191663+Jaydenasanto@users.noreply.github.com>
2022-07-09 12:31:57 +1000

    Exporting contents from files using index.ts structure
    this allows us to tidy up the code, not importing so many files. we can simply deconstruct the files that we need in the index.ts file we created

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,8 +1,6 @@
-import getAllProductsQuery from "../utils/queries/get-all-products";
-import fetchApi from "../utils/queries/fetch-api";
-import { normalizeProduct } from "../utils/normalize";
+import { fetchApi, normalizeProduct, getAllProductsQuery } from "../utils";
 import { ProductConnection } from "../schema";
-import { Product } from "../../common/types/product";
+import { Product } from "@common/types/product";
 
 type ReturnType = {
   products: ProductConnection;
