f31b177a04842b4c0e58805a3a8f290829558406
Jaydenasanto <83191663+Jaydenasanto@users.noreply.github.com>
2022-07-09 15:30:49 +1000

    Setting up deep-merge
    being able to merge between bigcommerce and shopify frameworks

--- next.config.js
+++ next.config.js
@@ -4,13 +4,18 @@ const { Console } = require("console");
 const nextConfig = {
   reactStrictMode: true,
 };
-
 module.exports = nextConfig;
-module.exports = {
+
+const { withFrameworkConfig } = require("./framework/common/config");
+
+module.exports = withFrameworkConfig({
+  framework: {
+    name: "shopify",
+  },
   i18n: {
     locales: ["en-US", "es"],
     defaultLocale: "en-US",
   },
-};
+});
 
 console.log("next.config.js", JSON.stringify(module.exports, null, 2));
