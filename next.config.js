179cbe69236ee7ce40b5c9937e687606a3248a7d
Jaydenasanto <83191663+Jaydenasanto@users.noreply.github.com>
2022-07-09 14:15:18 +1000

    Config files
    Shopify or Bigecommerce

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+const { Console } = require("console");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+module.exports = {
+  i18n: {
+    locales: ["en-US", "es"],
+    defaultLocale: "en-US",
+  },
+};
+
+console.log("next.config.js", JSON.stringify(module.exports, null, 2));
